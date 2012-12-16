9d6c7129218690debb4f88d2a84314259680dbca
FUJITAaki <wisteria.aki@gmial.com>
2012-12-16 23:41:51 +0900

    初期登録
    引き継ぎした時の状態で登録

---
+++ museum/2index.php
@@ -0,0 +1,188 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja"><!-- InstanceBegin template="/Templates/template.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>有馬切手文化博物館 博物館のご案内</title>
+<!-- InstanceEndEditable -->
+<meta name="keywords" content="有馬切手文化博物館,切手,有馬,有馬温泉,神戸,称徳館,馬事文化博物館,普通切手,記念切手,神戸消印" >
+<meta http-equiv="imagetoolbar" content="false" >
+<meta name="robots" content="index, follow" >
+<meta http-equiv="Content-Style-Type" content="text/css" >
+<meta http-equiv="Content-Script-Type" content="text/javascript" >
+<link rel="stylesheet" href="../base/base.css" type="text/css" media="all" >
+<script language="JavaScript" type="text/JavaScript" src="../base/base.js"></script>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+		<script type="text/javascript">
+$(function(){
+	$("ul.sub2").hide();
+	$("ul.menu2 li").hover(function(){
+		$("ul:not(:animated)",this).slideDown("fast");
+	},
+	function(){
+		$("ul",this).slideUp("fast");
+	});
+});
+		</script>
+		
+<link rel="stylesheet" href="../base/test.css" type="text/css" >
+
+
+
+
+<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
+<style type="text/css">
+</style>
+</head>
+<body onLoad="MM_preloadImages('../common/menu_4_o.gif','../common/menu_2_o.gif','../common/menu_3_o.gif','../common/menu_5_o.gif','../common/menu_6_o.gif','../common/menu_7_o.gif')">
+<div id="wrapper">
+	<div id="header"><a name="top"></a><img src="../common/header.jpg" alt="有馬切手文化博物館" width="760" height="107"></div>
+	
+
+
+<table width="760" border="0" cellpadding="0" cellspacing="0" id="main">
+		<tr>
+			<td width="180" align="right" id="menu"><div class="padding">
+			<a href="../index.html"><img src="../common/menu_1.gif" alt="ホーム" name="menu_1" width="143" height="28" border="0" id="menu_1"></a><a href="index.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('menu_2','','../common/menu_4_o.gif',1)"><img src="../common/menu_4.gif" alt="博物館のご案内" name="menu_2" width="143" height="28" border="0" id="menu_2"></a><a href="../tatemono/index.html" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image17','','../common/menu_2_o.gif',1)"><img src="../common/menu_2.gif" alt="建物の由来" name="Image17" width="143" height="28" border="0"></a><a href="../access/index.html" onMouseOver="MM_swapImage('menu_3','','../common/menu_3_o.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="../common/menu_3.gif" alt="交通のご案内" name="menu_3" width="143" height="28" border="0" id="menu_3"></a><a href="../guide/index.html" onMouseOver="MM_swapImage('menu_5','','../common/menu_5_o.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="../common/menu_5.gif" alt="展示ガイド" name="menu_5" width="143" height="28" border="0" id="menu_5"></a><a href="../links/index.html" onMouseOver="MM_swapImage('menu_6','','../common/menu_6_o.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="../common/menu_6.gif" alt="関連リンク集" name="menu_6" width="143" height="28" border="0" id="menu_6"></a><a href="../contact/index.html" onMouseOver="MM_swapImage('menu_7','','../common/menu_7_o.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="../common/menu_7.gif" alt="お問い合わせ" name="menu_7" width="143" height="28" border="0" id="menu_7" style="margin-bottom:8px"></a><a href="../tokubetsu/index.html"><img src="../common/bnr_tokubetsu.gif" alt="特別展のご案内" width="152" height="55" border="0"></a>
+			<!--<a href="../waribiki/index.html"><img src="../common/bnr_waribiki.gif" width="152" height="52" border="0" style="margin-bottom:14px"></a>--></div>
+				<!-- InstanceBeginEditable name="menu_image" --><img src="../common/menu_image.jpg" alt="" width="180" height="300"><!-- InstanceEndEditable --></td>
+			<td width="580" id="contents"><!-- InstanceBeginEditable name="contents" -->
+			
+			
+			
+			 <div class="padding">
+              <h1><img src="img/h1_title.gif" alt="博物館のご案内" width="550" height="47"></h1>
+              <div id="content">
+               <div class="main_image"><img src="img/main_image.jpg" alt="" width="505" height="118"></div>
+               <h2><img src="img/h2_title_raikan.gif" alt="来館のご案内" width="505" height="24"></h2>
+               <table border="0" cellpadding="0" cellspacing="10" class="hyo">
+                <tr>
+                 <th>入館料</th>
+                 <td>大人500円、中・高生200円、小学生以下無料<br>
+                  ※団体（15名以上）の場合はお問い合わせください<br>
+                 </td>
+                </tr>
+                <tr>
+                 <th>開館時間</th>
+                 <td>午前10時～午後４時（但し入館は午後３時30分まで）</td>
+                </tr>
+                <tr>
+                 <th>休館日</th>
+                 <td>火曜日（祝日の場合は翌日休）<br>
+				 ※収蔵品整理・展示準備などのため、臨時に休館する場合がございます。詳しくはお問い合わせください。</td>
+                </tr>
+                <tr>
+                 <th>駐車場</th>
+                 <td>34台（無料）</td>
+                </tr>
+                <tr>
+                 <th>住所</th>
+                 <td><a href="../access/index.html">→住所・地図のご案内はこちら</a></td>
+                </tr>
+               </table>
+               <p class="end_parag">当館では、車椅子の方でも自由にご覧いただけるようにバリアフリー対応です。<br>
+                <br>
+                <span class="color">★割引券を有馬温泉「観光総合案内所」にご用意しております。<br>
+                　 当博物館にお越しの際は是非ご利用ください。</span></p>
+			   <p class="to_top"><a href="#top">ページのトップへ↑</a></p>
+			   
+			 
+			   <h2><img src="../common/title_karenda.png" alt="博物館カレンダー" width="505" height="24"></h2>
+
+
+<img src="../common/callender_setumei.png"alt="アイコン説明">
+			
+
+
+
+
+<?php
+	require_once "../php_functions/calendar_functions.php";
+
+if (isset($_POST["mode"]) == true)
+{
+	if ($_POST["mode"] == "gyoji_preview")
+	{
+		$param = array("gyoji", $_POST["gyojikubun"], $_POST["anchor"], 
+						$_POST["kaishibi"], $_POST["shuryobi"], $_POST["k_cal"]);
+		echo calendarCreate(3, $param);
+	}
+	else if ($_POST["mode"] == "kyukambi_preview")
+	{
+		$param = array("kyukambi", $_POST["hizuke"]);
+		echo calendarCreate(3, $param);
+	}
+}
+else
+{
+	echo calendarCreate(3);
+}
+
+?>
+
+
+
+
+
+
+
+
+
+<p class="to_top"><a href="#top">ページのトップへ↑</a></p>
+
+
+
+			   
+			   
+               <h2><img src="img/h2_title_hanbai.gif" alt="記念品のご紹介" width="505" height="24"></h2>
+               <table border="0" cellpadding="0" cellspacing="0" class="hyo" style="margin-left:15px">
+                <tr>
+                 <td><img src="img/hanbai_img01.jpg" alt="オリジナル手さげ袋" width="164" height="112"></td>
+                 <td valign="top"><h4>●オリジナル手さげ袋　　<span class="color">500円</span></h4>
+                  様々な切手を散りばめた彩鮮やかな紙袋。<br>
+                 ちょっとした荷物を入れるのに便利な大きさなので、普段使いにどうぞ。</td>
+                </tr>
+                <tr>
+                 <td><img src="img/hanbai_img02.jpg" alt="日本の郵便切手 100種" width="164" height="112"></td>
+                 <td valign="top"><h4>●日本の郵便切手 100種　　<span class="color">500円</span></h4>
+                  日本の使用済み切手を100枚詰めたお得なセット。<br>
+                 あなただけの絵手紙を作ってみませんか？</td>
+                </tr>
+                <!--<tr>
+                 <td><img src="img/hanbai_img03.jpg" alt="切手文化博物館　ファーストデー・カバー" width="164" height="112"></td>
+                 <td valign="top"><h4>●切手文化博物館　ファーストデー・カバー<br>　
+                  <span class="color">300円</span></h4>
+                 博物館が開館した日付の消印が押されたオリジナル「ファーストデー・カバー」。</td>
+                </tr>-->
+                <tr>
+                 <td><img src="img/hanbai_img04.jpg" alt="切手タンブラー" width="164" height="112"></td>
+                 <td valign="top"><h4>●切手タンブラー（２個セット）　　<span class="color">600円</span></h4>
+                  「見返り美人」「月に雁」といった幻の切手をデザインしたタンブラーセット。<br>
+                  お気に入りの切手を眺めながらの一杯もなかなかのものです。</td>
+                </tr>
+               </table>
+               <table width="505" border="1" cellpadding="5" cellspacing="0" bordercolor="#70704D" id="kobanashi">
+                <tr>
+                 <td bgcolor="#70704D"><img src="../common/midashi_kobanashi.gif" alt="切手に関するコバナシ" width="131" height="14"></td>
+                </tr>
+                <tr>
+                 <td>1963年頃、海外の使用済み切手がグリコのおまけとして付けられ、小・中学生を中心に一大切手ブームをもたらした。</td>
+                </tr>
+               </table>
+			   <p class="to_top"><a href="#top">ページのトップへ↑</a></p>
+              </div>
+		    </div>
+			<!-- InstanceEndEditable --></td>
+		</tr>
+	</table>
+	<div id="footer"><img src="../common/footer.gif" alt="Copyright(C) 有馬切手文化博物館. All Rights Reserved." width="760" height="38"></div>
+</div>
+
+
+<?php	echo calendarCreate(3);
+
+?>
+</body>
+<!-- InstanceEnd --></html>
---
+++ test/indexarima1.php
@@ -0,0 +1,282 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>有馬切手文化博物館 ～郵便創業時から現代までの切手を網羅した常設博物館～</title>
+<meta name="keywords" content="有馬切手文化博物館,切手,有馬,有馬温泉,神戸,称徳館,馬事文化博物館,普通切手,記念切手,神戸消印" />
+<meta name="description" content="有馬切手文化博物館は、日本初の本格的な切手の常設博物館です。1871年に始まった日本郵便。そのすべての切手を、ここに展示しています。" />
+<meta http-equiv="imagetoolbar" content="false" />
+<meta name="robots" content="index, follow" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<script language="JavaScript" src="base/base.js"></script>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+		<script type="text/javascript">
+$(function(){
+	$("ul.sub").hide();
+	$("ul.menu li").hover(function(){
+		$("ul:not(:animated)",this).slideDown("fast");
+	},
+	function(){
+		$("ul",this).slideUp("fast");
+	});
+});
+		</script>
+
+
+
+<link rel="stylesheet" href="test.css" type="text/css" />
+
+
+
+<style type="text/css">
+<!--
+strong {
+	font-size: 1.17em}
+#info {
+	width: 760px;
+	background-image: url(common/body_bg2.gif)}
+#info td {
+	vertical-align: top}
+.tokubetsu {
+	background-image: url(topimg/tokubetsu_middle.gif);
+	background-repeat: repeat-y;
+	padding-left: 60px;
+	padding-right: 20px;
+	font-size: 0.75em;
+	line-height:130%;
+	}
+.no_margin{
+	padding-bottom:0px;
+	margin-bottom:0px;}
+
+	
+.copyright {
+	padding: 6px 0;
+	text-align: center}
+-->
+</style>
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<script type="text/JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+</head>
+<body onLoad="MM_preloadImages('topimg/menu_1_o.gif','topimg/menu_2_o.gif','topimg/menu_3_o.gif','topimg/menu_4_o.gif','topimg/menu_5_o.gif','topimg/menu_6_o.gif')">
+
+<div id="wrapper">
+  <div id="header"><img src="topimg/main_image_top.gif" alt="" width="760" height="17">
+  <script type="text/javascript">
+AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0','width','760','height','495','src','top','quality','high','pluginspage','http://www.macromedia.com/go/getflashplayer','movie','top' ); //end AC code
+	</script>
+    <noscript>
+    <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="760" height="495">
+      <param name="movie" value="top.swf">
+      <param name="quality" value="high">
+      <embed src="top.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="760" height="495"></embed>
+    </object>
+    </noscript>
+    <img src="topimg/main_image_bottom.gif" width="760" height="13"></div>
+  <div id="info">
+    <table width="760" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="409" style="height: 99px"><img src="newimg/1002button.gif" alt="博物館のご案内" name="menu_1" width="177" height="36" border="0" usemap="#menu_1map" id="menu_1"><a href="guide/index.html"><img src="topimg/menu_4.gif" alt="展示ガイド" name="menu_4" width="216" height="36" border="0" usemap="#menu_4map" id="menu_4"></a><br>
+          <a href="access/index.html">
+<map name="menu_2map" id="menu_2map">
+  <area shape="rect" coords="33,1,188,29" href="tatemono/index.html" onMouseOver="MM_swapImage('menu_2','','topimg/menu_2_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+</map>
+		<img src="topimg/menu_2.gif" alt="建物の由来" name="menu_2" width="195" height="31" border="0" usemap="#menu_2map" id="menu_2"></a><img src="topimg/menu_5.gif" alt="関連リンク集" name="menu_5" width="198" height="31" border="0" usemap="#menu_5map" id="menu_5"><br>
+          <img src="topimg/menu_3.gif" alt="交通のご案内" name="menu_3" width="220" height="32" border="0" usemap="#menu_3map" id="menu_3"><img src="topimg/menu_6.gif" alt="お問い合わせ" name="menu_6" width="173" height="32" border="0" usemap="#menu_6map" id="menu_6">
+          <map name="menu_6map" id="menu_6map">
+            <area shape="rect" coords="6,1,162,30" href="contact/index.html" onMouseOver="MM_swapImage('menu_6','','topimg/menu_6_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+          </map></td>
+      </tr>
+    </table>    
+         
+<div id="yotei">
+<div class="sotowaku">
+<div class="tuki">2012年<span class="number">10</span>月</div>
+<div class="retu_sun">日</div>
+<div class="retu_week">月</div>
+<div class="retu_week">火</div>
+<div class="retu_week">水</div>
+<div class="retu_week">木</div>
+<div class="retu_week">金</div>
+<div class="retu_week">土</div>
+<div class="retu_saisho">1</div>
+<div class="retu_hoka">2</div>
+<div class="retu_hoka">3</div>
+<div class="retu_hoka">4</div>
+<div class="retu_hoka">5</div>
+<div class="retu_hoka">6</div>
+<div class="retu_hoka">7</div>
+<div class="retu_saisho">8</div>
+<div class="retu_hoka">9</div>
+<div class="retu_hoka">10</div>
+<div class="retu_hoka">11</div>
+<div class="retu_hoka">
+
+	<ul class="menu">
+				<li><a href="#">12</a>
+					<ul class="sub">
+						<li><a href="http://www.kitte-museum-arima.jp/tokubetsu/index.html">特別展</a></li>
+						<li><a href="http://www.kitte-museum-arima.jp/info/index.html">絵はがき教室</a></li>
+
+					</ul>
+				</li></ul>
+
+
+</div>
+<div class="retu_hoka">13</div>
+<div class="retu_hoka">
+
+	<ul class="menu">
+				<li><a href="#">14</a>
+					<ul class="sub">
+						<li><a href="http://www.kitte-museum-arima.jp/tokubetsu/index.html">特別展</a></li>
+						<li><a href="http://www.kitte-museum-arima.jp/info/index.html">絵はがき教室</a></li>
+
+					</ul>
+				</li></ul>
+
+
+</div>
+<div class="retu_saisho">15</div>
+<div class="retu_hoka">16</div>
+<div class="retu_hoka">17</div>
+<div class="retu_hoka">18</div>
+<div class="retu_hoka">19</div>
+<div class="retu_hoka">20</div>
+<div class="retu_hoka">21</div>
+<div class="retu_saisho">22</div>
+<div class="retu_hoka">23</div>
+<div class="retu_hoka">24</div>
+<div class="retu_hoka">25</div>
+<div class="retu_hoka">26</div>
+<div class="retu_hoka">27</div>
+<div class="retu_hoka">28</div>
+<div class="retu_saisho">29</div>
+<div class="retu_hoka">30</div>
+<div class="retu_hoka">31</div>
+<div class="retu_hoka"></div>
+<div class="retu_hoka"></div>
+<div class="retu_hoka"></div>
+<div class="retu_hoka"></div>
+
+</div>
+
+<div class="icon">
+	<img src="file://Ws213/有馬切手文化博物館/素材/HP用/カレンダー用アイコン/45x45/krn_1-45.gif">
+	休館日<img src="file://Ws213/有馬切手文化博物館/素材/HP用/カレンダー用アイコン/45x45/krn_3-45.gif">絵はがき教室開講日</div>
+<div class="icon">特別展開催日イベント開催日</div>
+
+
+    <table>
+     <tr>      
+        <td style="height: 209px; width: 375px;"><div><img src="topimg/tokubetsu_top.gif" alt="新着情報" width="370" height="36" border="0"></div>
+          <div class="tokubetsu">
+          	<div style="width:295px; height:200px;overflow:auto" class="auto-style1">
+         	
+<?php
+require_once "php_functions/common_functions.php";
+
+
+
+
+$matsu = "(SELECT DATE_TRUNC('month',now() + '3 months') + '-1 days')"; 
+if (dbConnect($dbconn) == false)
+{
+	//エラー処理
+	exit(dbErrorMessageCreate("DB接続に失敗しました。"));
+}
+$sql = "SELECT * FROM kyukambi WHERE shinchaku_kokai=true and hizuke between(current_date)and $matsu order by hizuke;";
+//echo $sql. "<br>";
+
+$result = pg_query($dbconn, $sql);
+if ($result == false)
+{
+	//エラー処理
+	exit(dbErrorMessageCreate("DB抽出に失敗しました。", $sql, $dbconn));
+}
+
+//echo $sql;
+//echo "★";
+//		echo "データ件数：".pg_num_rows($result)."<br>\n";
+
+while ($DATA = pg_fetch_object($result)) {
+
+	echo $DATA->title. "<br>";
+	echo $DATA->comment. "<br>";
+
+}
+$sql = "SELECT * FROM gyoji WHERE shinchaku_kokai=true and kaishi_bi between(current_date)and $matsu and shuryo_bi between(current_date)and $matsu order by hyoji_yusendo;";
+//echo $sql. "<br>";
+
+$result = pg_query($dbconn, $sql);
+if ($result == false)
+{
+	//エラー処理
+	exit(dbErrorMessageCreate("DB抽出に失敗しました。", $sql, $dbconn));
+}
+
+while ($DATA = pg_fetch_object($result)) {
+	if ($DATA->gyoji_kubun == 1){ 
+	echo  "<a href = 'museum/index1.html'>".$DATA->title."</a><br>";
+	echo $DATA->comment. "<br>";
+	}
+	else if ($DATA->gyoji_kubun == 2){
+	echo  "<a href = 'museum/index2.html'>".$DATA->title."</a><br>";
+	echo $DATA->comment. "<br>";
+	}
+	else 
+		echo  "<a href = 'museum/index3.html'>".$DATA->title."</a><br>";
+	echo $DATA->comment. "<br>";
+	
+} 
+
+
+pg_close($dbconn);
+
+?>
+
+			
+	<a onClick="MM_openBrWindow('info/index.html','','scrollbars=yes,resizable=yes,width=600,height=500')" href="Javascript:;"><strong> 「切手で彩る絵はがき」教室開講</strong></a><br>
+          		※10月の開催日は、6日（土）、20日（土）です。</div>
+          </div>
+    
+          
+          <div>
+			  <img src="topimg/tokubetsu_bottom.gif" width="370" height="11" class="auto-style2"></div></td>
+      </tr>
+    </table>
+    </div>
+    <div class="copyright"><img src="topimg/copyright.gif" alt="Copyright(C) 有馬切手博物館. All Rights Reserved." width="251" height="11"></div>
+  </div>
+</div>
+<table width="760" border="0" align="center" cellpadding="0" cellspacing="5">
+  <tr>
+    <td><font size="1">※ムービーをご覧いただくにはFLASH
+      PLAYERが必要です。<br>
+      お持ちでない方はお手数ですが、右のバナーをクリックし、ダウンロードしてご覧ください。</font></td>
+    <td align="right"><a href="http://www.macromedia.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash&Lang=Japanese" target="_blank"><img src="topimg/get_flash_player.gif" alt="FLASH PLAYERをダウンロード" width="88" height="31" border="0"></a></td>
+  </tr>
+</table>
+<map name="menu_1map" id="menu_1map">
+  <area shape="rect" coords="10,6,166,35" href="museum/index.html" onMouseOver="MM_swapImage('menu_1','','topimg/menu_1_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+</map>
+<map name="menu_3map" id="menu_3map">
+  <area shape="rect" coords="58,2,213,30" href="access/index.html" onMouseOver="MM_swapImage('menu_3','','topimg/menu_3_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+</map>
+<map name="menu_4map" id="menu_4map">
+  <area shape="rect" coords="12,6,167,35" href="guide/index.html" onMouseOver="MM_swapImage('menu_4','','topimg/menu_4_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+</map>
+<map name="menu_5map" id="menu_5map">
+  <area shape="rect" coords="10,1,163,28" href="links/index.html" onMouseOver="MM_swapImage('menu_5','','topimg/menu_5_o.gif',1)" onMouseOut="MM_swapImgRestore()">
+</map>
+</body>
+</html>
